fc624590e878dadafb96302bf0e4d9e3cdd96ce4
burhanturan <99987473+burhanturan@users.noreply.github.com>
2022-10-17 01:06:15 +0300

    Delete testHover.spec.js

--- cypress/integration/testingTourradar/testHover.spec.js
+++
@@ -1,9 +0,0 @@
-
-
-describe('test hover',()=>{
-    it('test',()=>{
-        cy.visit('/')
-        cy.get('.js-ao-header-navigation__profile-placeholder').realHover()
-        cy.get('.ao-header-navigation__dropdown > .ao-header-navigation__dropdown-list--profile > .ao-header-navigation__dropdown-button-wrapper > .aa-btn--secondary').should('be.visible')
-    })
-})
