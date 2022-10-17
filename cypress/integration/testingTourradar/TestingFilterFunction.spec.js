893f1aa70f51544c18fc68b852503c5437043b37
Turan <burhanturan.elk@gmail.com>
2022-10-17 12:40:17 +0300

    Tourradar Automation Testing is completed

--- cypress/integration/testingTourradar/TestingFilterFunction.spec.js
+++ cypress/integration/testingTourradar/TestingFilterFunction.spec.js
@@ -11,65 +11,79 @@ describe('Users should be able to filter the tours and trips', () => {
 
     it('Testing the filtering tours and trips', () => {
 
-        let selectedFilters = []
-        let text = ""
+        var selectedFilters = new Array();
 
-        //Adventure Type
-        cy.get('[for="checkbox-group-type-192"]').click()
-        cy.get('[for="checkbox-group-type-192"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
-        })
-        selectedFilters.push(text)
 
-        //Guide Type
-        cy.get('[for="checkbox-guide-type-373"]').click()
-        cy.get('[for="checkbox-guide-type-373"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
-        })
-        selectedFilters.push(text)
+        //Adventure Type (Group)
+        //cy.get('[for="checkbox-group-type-192"]').click()
+        cy.get('[for="checkbox-group-type-372"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
 
-        //Group Size
-        cy.get('[for="checkbox-group-sizes-up_to_20"]').click()
-        cy.get('[for="checkbox-group-sizes-up_to_20"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
         })
-        selectedFilters.push(text)
+      
 
-        //Adventure Style
-        cy.get('[for="checkbox-travel-substyles-383"]').click()
-        cy.get('[for="checkbox-travel-substyles-383"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
+        //Guide Type (Fully Guided)
+        //cy.get('[for="checkbox-guide-type-373"]').click()
+        cy.get('[for="checkbox-guide-type-373"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
         })
-        selectedFilters.push(text)
 
-        //Must See Countryies
-        cy.get('[for="checkbox-must-see-countries-66"]').click()
-        cy.get('[for="checkbox-must-see-countries-66"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
+        //Group Size (20 People)
+        //cy.get('[for="checkbox-group-sizes-up_to_20"]').click()
+        cy.get('[for="checkbox-group-sizes-up_to_20"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
         })
-        selectedFilters.push(text)
 
-        //Physical Rating
-        cy.get('[for="checkbox-physical-rating-377"]').click()
-        cy.get('[for="checkbox-physical-rating-377"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
+        //Adventure Style (Explorer)
+        //cy.get('[for="checkbox-travel-substyles-383"]').click()
+        cy.get('[for="checkbox-travel-substyles-383"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
         })
-        selectedFilters.push(text)
 
-        //Operated in
-        cy.get('[for="checkbox-guide-language-1"]').click()
-        cy.get('[for="checkbox-guide-language-1"]').invoke('text').then((textOfFilter) => {
-            text = textOfFilter
+        //Must See Countryies (Germany)
+        //cy.get('[for="checkbox-must-see-countries-66"]').click()
+        cy.get('[for="checkbox-must-see-countries-66"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
         })
-        selectedFilters.push(text)
+
+        //Physical Rating (Easy)
+        //cy.get('[for="checkbox-physical-rating-377"]').click()
+        cy.get('[for="checkbox-physical-rating-377"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
+        })
+
+        //Operated in (English)
+        //cy.get('[for="checkbox-guide-language-1"]').click()
+        cy.get('[for="checkbox-guide-language-1"]').each(($el) => {
+            var text = $el.text()
+            selectedFilters.push(text)
+            cy.wrap($el).click()
+        })
+
 
 
         //Assertion of selected filters
-        cy.get('[class="js-serp-parameters__filters-filter serp-parameters__filters-filter cf"]').each(($el, index) => {
+        cy.get('[class="js-serp-parameters__filters serp-parameters__filters cf"]').each(($el, index) => {
 
-            let text = $el.text()
+            const text = $el.text()
             expect(text).to.contains(selectedFilters[index])
 
+            for (let i = 0; i < selectedFilters.length; i++) {
+                expect(text).to.contain(selectedFilters[i])
+            }
+            
         })
 
         //Removing all selected filters
